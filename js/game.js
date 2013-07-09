24f3a489122355a46f972e223cb9d16022349e54
Antonio Prada <toniprada@gmail.com>
2013-07-09 12:15:18 +0300

    Starting to work on the torchlight

---
+++ js/game.js
@@ -0,0 +1,52 @@
+player = undefined;
+
+/* Game namespace */
+var game = {
+    // Run on page load.
+    "onload" : function () {
+        // Initialize the video.
+        if (!me.video.init("screen", 480, 320, true, 'auto')) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
+		
+		// add "#debug" to the URL to enable the debug Panel
+		if (document.location.hash === "#debug") {
+			window.onReady(function () {
+				me.plugin.register.defer(debugPanel, "debug");
+			});
+		}
+
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
+
+        // Set a callback to run when loading is complete.
+        me.loader.onload = this.loaded.bind(this);
+     
+        // Load the resources.
+        me.loader.preload(game.resources);
+
+        // Initialize melonJS and display a loading screen.
+        me.state.change(me.state.LOADING);
+    },
+
+
+
+    // Run on game resources loaded.
+    "loaded" : function () {
+   // set the "Play/Ingame" Screen Object
+   me.state.set(me.state.PLAY, new game.PlayScreen());
+     
+   // add our player entity in the entity pool
+   me.entityPool.add("mainPlayer", game.PlayerEntity);
+   me.entityPool.add("torchlight", game.LightEntity);
+ 
+   // enable the keyboard
+   me.input.bindKey(me.input.KEY.LEFT,  "left");
+   me.input.bindKey(me.input.KEY.RIGHT, "right");
+   me.input.bindKey(me.input.KEY.X,     "jump");
+      
+   // start the game 
+   me.state.change(me.state.PLAY);
+    }
+};
---
+++ js/resources.js
@@ -0,0 +1,47 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+	
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+	
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+
+    /**
+     * Graphics.
+     */
+    // our level tileset
+    {name: "area01_level_tiles", type:"image", src: "data/img/map/area01_level_tiles.png"},
+    // the main player spritesheet
+    {name: "gripe_run_right", type:"image", src: "data/img/sprite/gripe_run_right.png"},
+    {name: "light_mask", type:"image", src: "data/img/sprite/light_mask.png"},
+
+    // the parallax background
+    {name: "area01_bkg0",         type:"image", src: "data/img/area01_bkg0.png"},
+    {name: "area01_bkg1",         type:"image", src: "data/img/area01_bkg1.png"},
+
+    /* 
+     * Maps. 
+     */
+    {name: "area01", type: "tmx", src: "data/map/area01.tmx"}
+
+];
